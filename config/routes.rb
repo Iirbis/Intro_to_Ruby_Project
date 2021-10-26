dc10ccf0232b51325343f452199c62a3551d5ad5
Stacie <pathfinder.em@gmail.com>
2021-10-25 23:19:05 -0500

    Added home directory controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
   resources :recipes, only: [:index, :show]
   resources :categories, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
