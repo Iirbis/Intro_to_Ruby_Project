2082df1ed80118a742242076ca51d7820775ea22
Stacie <pathfinder.em@gmail.com>
2021-10-26 00:33:05 -0500

    Added pagination to Recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(:category).all
+    @recipes = Recipe.includes(:category).paginate(:page => params[:page], :per_page => 30)
   end
 
   def show
