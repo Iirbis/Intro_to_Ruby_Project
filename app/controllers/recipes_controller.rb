d750abff2639df978eaeb35b20b2ab6ecec5ab45
Stacie <pathfinder.em@gmail.com>
2021-10-27 11:12:20 -0500

    Search feature implemented

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,14 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(:category).paginate(:page => params[:page], :per_page => 30)
+    # @recipes = Recipe.includes(:category).paginate(:page => params[:page], :per_page => 30)
+    if params[:search]
+      @recipes = Recipe.where('title LIKE ?', "%#{params[:search]}%").paginate(:page => params[:page], :per_page => 30)
+    else
+      @recipes = Recipe.includes(:category).paginate(:page => params[:page], :per_page => 30)
   end
 
   def show
     @recipe = Recipe.find(params[:id])
   end
+end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,6 +8,20 @@ class Recipe < ApplicationRecord
   validates :title, uniqueness:true
 
 
+  def recipe_params
+    params.require(:recipe).permit(:title, :directions, :search)
+  end
 
-
+  def self.search(search)
+    if search
+      recipe = Recipe.find_by(title: search)
+      if recipe
+        self.where(title: recipe)
+      else
+        Recipe.all
+      end
+    else
+      Recipe.all
+    end
+  end
 end
