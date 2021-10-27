bb9b51fb508d4352f089cd005481a54eb2bcbb46
Stacie <pathfinder.em@gmail.com>
2021-10-27 16:51:06 -0500

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "about", to: "about#index"
   root to: "home#index"
   resources :recipes, only: [:index, :show]
   resources :categories, only: [:index, :show]
